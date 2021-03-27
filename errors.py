79921c51ecfbef06c1ba994aceeeba3bd5236891
caughtquick <62573491+caughtquick@users.noreply.github.com>
2021-03-27 16:13:24 -0700

    Done requested changes
    Removed print statements, fixed grammatical errors

--- errors.py
+++ errors.py
@@ -1,22 +1,15 @@
 class TwirlErrors(Exception):
     pass
 class DownloadError(TwirlErrors):
-    print("Package failed to download")
     pass
 class ChecksumDoesntMatch(TwirlErrors):
-    print("Package checksum doesn't match, retry")
     pass
 class PackageNotFound(TwirlErrors):
-    print("Package unable to be found")
     pass
 class PackageInstallFailed(TwirlErrors):
-    print("Package failed to install")
     pass
 class PackageAlreadyInstalled(TwirlErrors):
-    print("Package is already installed")
     pass
 class PackageConflicts(TwirlErrors):
-    #needs to find and print conflicting packages
-    print("Package conflicts with other installed package")
     pass
 
