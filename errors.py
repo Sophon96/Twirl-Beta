26ea1a66dfc16547e593b025736cbeb33a36f27b
Sophon96 <71684640+Sophon96@users.noreply.github.com>
2021-03-27 16:25:43 -0700

    Add comments to each exception class.

--- errors.py
+++ errors.py
@@ -1,15 +1,33 @@
 class TwirlErrors(Exception):
-    pass
-class DownloadError(TwirlErrors):
-    pass
-class ChecksumDoesntMatch(TwirlErrors):
-    pass
-class PackageNotFound(TwirlErrors):
-    pass
-class PackageInstallFailed(TwirlErrors):
-    pass
-class PackageAlreadyInstalled(TwirlErrors):
-    pass
-class PackageConflicts(TwirlErrors):
+    """Base class for all Twirl errors."""
     pass
 
+
+class DownloadError(TwirlErrors):
+    """The target package failed to download."""
+    pass
+
+
+class ChecksumDoesntMatch(TwirlErrors):
+    """The checksum of the tarball doesn't match."""
+    pass
+
+
+class PackageNotFound(TwirlErrors):
+    """The target package wasn't found/doesn't exist."""
+    pass
+
+
+class PackageInstallFailed(TwirlErrors):
+    """The target package failed to install."""
+    pass
+
+
+class PackageAlreadyInstalled(TwirlErrors):
+    """The target package is already installed."""
+    pass
+
+
+class PackageConflicts(TwirlErrors):
+    """Target package conflicts with an already installed package."""
+    pass
