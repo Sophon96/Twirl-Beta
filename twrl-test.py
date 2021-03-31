8de39b32a9847f34bec01783e5e6c13a70ceeb20
nature174 <53713683+nature174@users.noreply.github.com>
2021-03-31 13:33:20 -0700

    New package format and file extraction (WIP)

--- twrl-test.py
+++ twrl-test.py
@@ -6,8 +6,11 @@ print(f"{data.name} | {fp}\n\n")
 for member in data:
 	print(f"{member.name} | Type: {'File' if member.isfile() else 'Folder'} | Size: {member.size}\n")
 
-stuff = data.extractfile("neotwirl/pkginfo/PKGINFO")
+#stuff = data.extractfile("pkginfo/PKGINFO")
 print("PKGINFO:")
-pkg = p.pkginfo(stuff)
+pkg = p.pkginfo()
 print(pkg)
 print(f"\nPackage Name: {pkg['info']['Package']}")
+q = input("Extract [y]? ")
+if q=="y":
+    print(p.extract_rootfs("./out/"))
--- twrlparse.py
+++ twrlparse.py
@@ -32,9 +32,10 @@ class Parser:
         return [self.tmpdir + self.pkg.split(".")[0] + ".tar", self.tarfile]
 
 
-    def pkginfo(self, tar_file : tarfile.TarFile):
-        stuff = tar_file.read().strip().replace(b"\n", b"")
-        pkginfo_data = json.loads(stuff)
+    def pkginfo(self):
+        stuff = self.tarfile.extractfile("pkginfo/PKGINFO")
+#        stuff = self.tarfile.read().strip().replace(b"\n", b"")
+        pkginfo_data = json.load(stuff)
         return pkginfo_data
 
     def extract_rootfs(self,dest):
@@ -42,4 +43,7 @@ class Parser:
             pass
         else:
             raise Exception(f"{dest} is not a valid folder.")
+        tf = self.tarfile
+        res = tf.extractall(path=dest)
+        return dest + "/" if dest[-1] != "/" else "" + self.pkg.split(".")[0]
         # use self.tarfile and extract to {dest}
