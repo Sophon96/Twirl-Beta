9b2e3dd446f74b897827f8cc64369cc07adf3a74
nature174 <53713683+nature174@users.noreply.github.com>
2021-04-14 16:33:51 -0700

    Beautify and improve code

--- twrl-test.py
+++ twrl-test.py
@@ -1,13 +1,16 @@
 import twrlparse as tp
 from sys import argv
-p = tp.Parser("neotwirl.tar.zst","./tmp")
+
+p = tp.Parser("neotwirl.tar.zst", "./tmp")
 fp, data = p.load()
 print(f"{data.name} | {fp}\n\n")
 
 for member in data:
-	print(f"{member.name} | Type: {'File' if member.isfile() else 'Folder'} | Size: {member.size}\n")
+    print(
+        f"{member.name} | Type: {'File' if member.isfile() else 'Folder'} | Size: {member.size}\n"
+    )
 print(data)
-#stuff = data.extractfile("pkginfo/PKGINFO")
+# stuff = data.extractfile("pkginfo/PKGINFO")
 print("PKGINFO:")
 pkg = p.pkginfo()
 print(pkg)
--- twrlparse.py
+++ twrlparse.py
@@ -4,16 +4,20 @@ import zstandard as zstd
 import os
 import shutil
 
+
 class Parser:
     """Parse twirl package files"""
 
-    def __init__(self, package_location, tmpdir="/tmp/twrl-parse."+str(os.getpid())):
+    def __init__(
+        self, package_location: str, tmpdir: str = "/tmp/twrl-parse." + str(os.getpid())
+    ):
         """Package location and tmpdir"""
         if os.path.isfile(package_location):
             self.pkg = package_location
         else:
             raise FileNotFoundException(
-                f"{package_location} is not a valid twirl package file.")
+                f"{package_location} is not a valid twirl package file."
+            )
         if os.path.isdir(tmpdir):
             xtraslash = "/" if tmpdir[-1] != "/" else ""
             self.tmpdir = tmpdir + xtraslash
@@ -21,37 +25,40 @@ class Parser:
             status_code = os.system(f"mkdir -p {tmpdir}")
             if status_code != 0:
                 raise FileNotFoundException(
-                    f"{tmpdir} is not a valid directory. (Error {status_code})")
+                    f"{tmpdir} is not a valid directory. (Error {status_code})"
+                )
 
-    def load(self):
-        """write it"""
+    def load(self) -> list:
+        """Extract File to Temp Dir"""
         dctx = zstd.ZstdDecompressor()
-        with open(self.pkg, "rb") as zst, open(self.tmpdir + self.pkg.split(".")[0] + ".tar", "wb") as dest:
+        with open(self.pkg, "rb") as zst, open(
+            self.tmpdir + self.pkg.split(".")[0] + ".tar", "wb"
+        ) as dest:
             dctx.copy_stream(zst, dest)
         self.tarfile = tarfile.open(self.tmpdir + self.pkg.split(".")[0] + ".tar")
         return [self.tmpdir + self.pkg.split(".")[0] + ".tar", self.tarfile]
 
-
-    def pkginfo(self):
+    def pkginfo(self) -> dict:
+        """Get info about the package (run load() first)"""
         stuff = self.tarfile.extractfile("pkginfo/PKGINFO")
-#        stuff = self.tarfile.read().strip().replace(b"\n", b"")
         pkginfo_data = json.load(stuff)
         return pkginfo_data
 
-    def extract_rootfs(self,dest):
+    def extract_rootfs(self, dest: str) -> str:
+        """Extract the package's contents to location specified in dest"""
         if os.path.isdir(dest):
             pass
         else:
             raise Exception(f"{dest} is not a valid folder.")
         tf = self.tarfile
         subdir_and_files = [
-        tarinfo for tarinfo in tf.getmembers()
-        if tarinfo.name.startswith("data/") and len(tarinfo.name) > 5
+            tarinfo
+            for tarinfo in tf.getmembers()
+            if tarinfo.name.startswith("data/") and len(tarinfo.name) > 5
         ]
-        tf.extractall(members=subdir_and_files,path=self.tmpdir)
-#        os.system(f"""bash -c "mv {self.tmpdir}data/* {dest}";ls {dest}""")
+        tf.extractall(members=subdir_and_files, path=self.tmpdir)
         src = self.tmpdir + "data/"
         files = os.listdir(src)
         for file in files:
-            shutil.move(src + file,dest)
+            shutil.move(src + file, dest)
         return dest
